648b79657d509f6ae3ec630932f4b392c04eda88
DipanNama <dipannama91@gmail.com>
2026-05-13 21:23:23 +0530

    feat: updated testimonial photos section

--- js/custom.js
+++ js/custom.js
@@ -136,6 +136,29 @@ $(document).ready(function() {
       },
     });
 
+    var swiper = new Swiper(".image_testi_area", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        }
+      }
+    });
+
     var swiper = new Swiper(".client-area", {
       slidesPerView: 6,
 	  slidesPerGroup: 6,
